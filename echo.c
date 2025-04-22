7e32136c6783ca5fe40fde797b3e35ebc14239a0
Cory (Riley) Black <32332593+RileyMeta@users.noreply.github.com>
2025-04-22 14:37:31 -0400

    Update echo.c
    + Added `-n` suppression of newline characters

--- echo.c
+++ echo.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char* argv[]) {
-    for (int i = 1; i < argc; ++i) {
-        // Simple setup for CLI Args
-        if (strcmp(argv[i], "-V") == 0) {
-            printf("Success");
-            return 0;
-        }
+    int start = 1;
+    bool suppress = false;
 
+    if (argc > 1) {
+        if (strcmp(argv[1], "-n") == 0) {
+            suppress = true;
+            start = 2;
+        }
+    }
+
+    for (int i = start; i < argc; ++i) {
         printf("%s", argv[i]);
         if (i < argc - 1) {
             printf(" ");
         }
     }
-    printf("\n");
+
+    if (!suppress) {
+        printf("\n");
+    }
     return 0;
 }
