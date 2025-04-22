891fb8034f9e194e900c718edf676b54b7c1fb16
Cory (Riley) Black <32332593+RileyMeta@users.noreply.github.com>
2025-04-22 14:28:57 -0400

    Update echo.c

--- echo.c
+++ echo.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; ++i) {
+        // Simple setup for CLI Args
+        if (strcmp(argv[i], "-V") == 0) {
+            printf("Success");
+            return 0;
+        }
+
         printf("%s", argv[i]);
         if (i < argc - 1) {
             printf(" ");
